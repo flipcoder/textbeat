4ad31c5d3329a76a18bf03678e80acc2145b590e
Grady O'Connell <flipcoder@gmail.com>
2018-05-18 22:18:55 -0700

    almost

--- decadence.py
+++ decadence.py
@@ -4,6 +4,7 @@ import sys
 import time
 import pygame
 import pygame.midi as midi
+import traceback
 
 # SCALES = [
 #     [
@@ -36,80 +37,114 @@ class Scale:
     HARMONIC_MINOR = 3
     MELODIC_MINOR = 4
     CHROMATIC = 5
-    def __init__(self, name, intervals, modes):
+    def __init__(self, name, intervals): # modes)
         self.name = name
         self.intervals = intervals
-        self.modes = modes
+        # self.modes = modes
 
 SCALE_NAMES.append('diatonic')
 SCALES.append(Scale(
-    'diatonic', '2212221',
-    [   "ionian",
-        "dorian",
-        "phyrigian",
-        "lydian",
-        "mixolydian",
-        "aeolian",
-        "locrian" ]
+    'diatonic', '2212221'
+    # [   "ionian",
+    #     "dorian",
+    #     "phyrigian",
+    #     "lydian",
+    #     "mixolydian",
+    #     "aeolian",
+    #     "locrian" ]
 ))
 
+RANGE = 109
+
 class Channel:
-    def __init__(self, player):
-        self.notes = [0] * 255
+    def __init__(self, ch, player):
+        self.ch = ch # channel number
+        self.notes = [0] * RANGE
         self.scale = Scale.DIATONIC
         self.player = player
         self.instrument = 0
-    def midi_note_on(self, n, v):
+    def note_on(self, n, v):
+        if n < 0 or n > RANGE:
+            return
         self.notes[n] = v
-        PLAYER.note_on(n,v)
-    def midi_note_off(self, n, v=127):
+        self.player.note_on(n,v,self.ch)
+    def note_off(self, n, v=127):
+        if n < 0 or n >= RANGE:
+            return
         if self.notes[n]:
-            PLAYER.note_off(n,v)
-            self.notes[n] = v
+            self.player.note_off(n,v,self.ch)
+            self.notes[n] = 0
+    def note_all_off(self, v=127):
+        for n in xrange(RANGE):
+            self.player.note_off(n,v,self.ch)
+        self.notes = [0] * RANGE
 
 midi.init()
-PLAYER = pygame.midi.Output(0)
+PLAYER = pygame.midi.Output(pygame.midi.get_default_output_id())
 INSTRUMENT = 0
 PLAYER.set_instrument(INSTRUMENT)
-CHANNELS = [Channel(PLAYER) for x in range(16)]
+CHANNELS = [Channel(x, PLAYER) for x in range(16)]
 
 try:
     with open(sys.argv[1]) as f:
         for line in f.readlines():
             cells = line.split('|')
             ch_idx = 0
+            if not line.strip():
+                continue
+            print line[:-1]
             for cell in cells:
-                for note in ch.notes:
-                    PLAYER.note_off(note,127)
                 ch = CHANNELS[ch_idx]
-                if INSTRUMENT != ch.instrument:
-                    PLAYER.set_instrument(INSTRUMENT)
-                    INSTRUMENT = ch.instrument
+                # if INSTRUMENT != ch.instrument:
+                #     PLAYER.set_instrument(ch.instrument)
+                #     INSTRUMENT = ch.instrument
                 cell = cell.strip()
+                if not cell:
+                    ch_idx += 1
+                    continue
+                ch.note_all_off()
+                
+                if cell=='-': # mute
+                    continue
+                
                 scale = SCALES[ch.scale]
                 notecount = len(scale.intervals)
                 # octave = int(line[0]) / notecount
                 note = ((int(line[0])-1) % notecount) + 1
-                p = 0
+                n = 0
                 for i in xrange(note):
-                    p += int(scale.intervals[i-1])
-                mn = 64 + int(p) # midi note
-                if line[1] == '\'':
-                    mn += 12
-                elif line[1] == '.':
-                    mn -= 12
-                ch.midi_note_on(mn,127)
+                    n += int(scale.intervals[i-1])
+                octave = 5
+                base = 4
+                mn = 0
+                
+                mn = n + base + octave * 12
+                
+                if len(cell) >= 3:
+                    if cell[1] == '+':
+                        shift = int(cell[2]) if cell[2].isdigit() else 0
+                        mn = n + base + octave * 12 + shift * 12
+                    elif cell[1] == '-':
+                        shift = int(cell[2]) if cell[2].isdigit() else 0
+                        mn = n + base + octave * 12 - shift * 12
+                    elif cell[1] == '=':
+                        shift = int(cell[2]) if cell[2].isdigit() else 0
+                        mn = n + base + shift * 12
+                
+                ch.note_on(mn,127)
                 
                 ch_idx += 1
 
-            if not line.strip():
+            if line.strip():
                 time.sleep(0.2)
 
+    time.sleep(0.2)
+
 except Exception, ex:
-    print ex
-    for ch in CHANNELS:
-        for note in ch.notes:
-            PLAYER.note_off(note,127)
+    print traceback.format_exc(ex)
+
+for ch in CHANNELS:
+    ch.note_all_off()
 
 del PLAYER
 midi.quit()

348382a24849818714f0b2a15aec9a2e85a56533
Grady O'Connell <flipcoder@gmail.com>
2018-05-19 17:18:39 -0700

    too many changes to name! rapid prototyping!

--- decadence.py
+++ decadence.py
@@ -55,9 +55,12 @@ CHORDS = {
     "q": "4 b7", # quartal
     "qt": "5 9", # quintal
     "mu": "2 3 5", # maj add2
-    "mu-": "2 b3 5", # min add2
+    "mu7": "2 3 5", # maj add2
+    "mu-": "2 b3 5", # m add2
+    "mu-7": "2 b3 5 7", # m7 add2
     "wa": "3 4 5", # maj add4
-    "wa-": "b3 4 5", # min add4
+    "wa7": "3 4 5 7", # maj7 add4
+    "wa-7": "b3 4 5 7", # m7 add4
     "lyd": "3 #4",
 }
 CHORDS_ALT = {
@@ -96,31 +99,43 @@ def expand_chord(c):
     return CHORDS[normalize_chord(c)].split(' ')
 
 RANGE = 109
-DEFAULT_OCTAVE = 5
+OCTAVE_BASE = 5
 
 class Channel:
     def __init__(self, ch, player):
-        self.ch = ch # index in CHANNELS list
+        self.ch = ch
+        self.player = player
+        self.reset()
+    def reset(self):
         self.midich = 0
         self.notes = [0] * RANGE
         self.mode = 1 # 0 is NONE which inherits global mode
         self.scale = Scale.DIATONIC
-        self.player = player
         self.instrument = 0
-        self.octave = DEFAULT_OCTAVE
+        self.octave = 0 # rel to OCTAVE_BASE
         self.mod = 0 # dont read in mod, just track its change by this channel
-    def note_on(self, n, v):
+        self.hold = False
+        self.arp = [] # list of notes to arpegiate
+        self.pattern = [] # relative steps to
+        self.vel = 0
+    def note_on(self, n, v=-1):
+        if v == -1:
+            v = self.vel
         if n < 0 or n > RANGE:
             return
         self.notes[n] = v
         self.player.note_on(n,v,self.midich)
-    def note_off(self, n, v=127):
+    def note_off(self, n, v=-1):
+        if v == -1:
+            v = self.vel
         if n < 0 or n >= RANGE:
             return
         if self.notes[n]:
             self.player.note_off(n,v,self.midich)
             self.notes[n] = 0
-    def note_all_off(self, v=127): # this is not midi all off, just object
+    def note_all_off(self, v=-1): # this is not the midi equivalent
+        if v == -1:
+            v = self.vel
         for n in xrange(RANGE):
             if self.notes[n]:
                 self.player.note_off(n,v,self.midich)
@@ -160,7 +175,7 @@ class StackFrame:
 BOOKMARKS = {}
 CALLSTACK = []
 
-CCHAR = '<>=~.\'\`,'
+CCHAR = '<>=~.\'\`,_'
 
 try:
     slept = True
@@ -178,7 +193,7 @@ try:
                 ls = line.strip()
                 
                 # place bookmark
-                if ls[-1]==':':
+                if ls and ls[-1]==':':
                     # only store INITIAL bookmark position here
                     if not ls[:-1] in BOOKMARKS:
                         BOOKMARKS[ls[:-1]] = len(buf)
@@ -195,12 +210,16 @@ try:
         # cells = ' '.join(line.split(' ')).split(' ')
         cells = line.split(' '*2)
         cells = filter(None, cells)
-        print cells
         ch_idx = 0
         # if not line.strip():
         #     continue
 
         notes = []
+
+        # COMMENT
+        if buf and buf[row][0] == ';':
+            row += 1
+            continue
         
         ctrl = False # ctrl line, %
         for cell in cells:
@@ -260,10 +279,15 @@ try:
                     row = BOOKMARKS[bm]
                     continue
             
-            if cell[0]=='-' or cell[0]=='=': # mute
+            if cell=='-' or cell[0]=='=': # mute
                 ch.note_all_off()
                 ch_idx += 1
                 continue
+
+            if cell[0]=='-':
+                ch.note_all_off()
+                ch.hold = False
+                cell = cell[1:]
             
             scale = SCALES[ch.scale]
             notecount = len(scale.intervals)
@@ -324,24 +348,26 @@ try:
                 
                 elif c in 'b#ABCDEFG':
                     
-                    c = tok[0]
                     # flats, sharps after note names?
-                    if len(tok) > 2 and tok[1:3] =='bb':
-                        n -= 2
-                        tok = tok[2:]
-                        if not expanded: cell = cell[2:]
-                    elif c == 'b':
-                        n -= 1
-                        tok = tok[1:]
-                        if not expanded: cell = cell[1:]
-                    elif len(tok) > 2 and tok[1:3] =='##':
-                        n += 2
-                        tok = tok[2:]
-                        if not expanded: cell = cell[2:]
-                    elif c =='#':
-                        n += 1
-                        tok = tok[1:]
-                        if not expanded: cell = cell[1:]
+                    if tok:
+                        lt = len(tok)
+                        if lt > 2 and tok[1:3] =='bb':
+                            n -= 2
+                            tok = tok[:2]
+                            if not expanded: cell = cell[:2]
+                        elif lt > 1 and tok[1] == 'b':
+                            n -= 1
+                            tok = tok[:1]
+                            if not expanded: cell = cell[:1]
+                        elif lt > 2 and tok[1:3] =='##':
+                            n += 2
+                            tok = tok[:2]
+                            if not expanded: cell = cell[:2]
+                        elif lt > 1 and tok[1] =='#':
+                            n += 1
+                            tok = tok[:1]
+                            if not expanded: cell = cell[:1]
+                    
                     c = tok[0]
 
                     # note names, don't use these in chord defn
@@ -350,7 +376,6 @@ try:
                         note = ' CDEFGAB'.index(c)
                         for i in xrange(note):
                             n += int(DIATONIC.intervals[i-1])
-                        # print tok
                         tok = tok[1:]
                         if not expanded: cell = cell[1:]
                     except ValueError:
@@ -371,7 +396,6 @@ try:
                     
                     # this will continue looping to process notes
                     if chordname:
-                        print  "chord? " + chordname
                         try:
                             cell = cell[len(chordname):] 
                             chord_notes = expand_chord(chordname)
@@ -386,14 +410,14 @@ try:
                 
                 notes.append(n + chord_root-1)
             
-            base = 4
+            base = 4 + OCTAVE_BASE * 12
             p = base + octave * 12 # default
             
             cell = cell.strip() # ignore spaces
 
+            vel = 127
             nomute = False
             while len(cell) >= 1:
-                print 'cmd: ' + cell
                 # All tokens here must be listed in CCHAR
                 
                 ## + and - symbols are changed to mean minor and aug chords
@@ -407,12 +431,12 @@ try:
                 #     cell = cell[1:] # deprecated, ignore
                     # continue
 
+                # OCTAVE SHIFT UP
                 if c == '>' or c=='\'' or c=='\`':
                     sym = c
                     cell = cell[1:]
-                    c = cell[0]
-                    if c.isdigit():
-                        shift = int(c)
+                    if cell and cell[0].isdigit():
+                        shift = int(cell[0])
                         cell = cell[1:]
                     else:
                         shift = 1
@@ -424,12 +448,12 @@ try:
                 #     c = cell[1]
                 #     shift = int(c) if c.isdigit() else 0
                 #     p = base + (octave+shift) * 12
+                # OCTAVE SHIFT DOWN
                 elif c == '<' or c==',':
                     sym = c
                     cell = cell[1:]
-                    c = cell[0]
-                    if c.isdigit():
-                        shift = int(c)
+                    if cell and cell[0].isdigit():
+                        shift = int(cell[0])
                         cell = cell[1:]
                     else:
                         shift = 1
@@ -438,35 +462,52 @@ try:
                     if sym == '<':
                         ch.octave = octave # persist
                     # row_events += 1
+                # SET OCTAVE
                 elif c == '=':
                     cell = cell[1:]
-                    c = cell[0]
-                    if c.isdigit():
-                        octave = int(c)
+                    if cell and cell[0].isdigit():
+                        octave = int(cell[0])
                         cell = cell[1:]
                     else:
-                        octave = DEFAULT_OCTAVE
+                        octave = 0 # default
                         shift = 1
                     ch.octave = octave
                     p = base + octave * 12
                     # row_events += 1
+                # VIBRATO
                 elif c == '~': # vibrato -- eventually using pitch wheel
                     ch.cc(1,127)
                     cell = cell[1:]
                     nomute = True
                     # row_events += 1d
+                # SEP
                 elif c=='.':
                     cell = cell[1:] #ignore
+                    nomute = True
+                # HOLD
                 elif c=='_':
                     ch.hold = True
                     cell = cell[1:]
-            
-            if notes and not nomute:
+                    assert notes # holding w/o note?
+                elif c=='v':
+                    cell = cell[1:]
+                    # get number
+                    num = ''
+                    for char in cell:
+                        if char.isdigit():
+                            num += char
+                        else:
+                            break
+                    assert num != ''
+                    vel = int(num) / ('9'*len(num))
+                    ch.vel = vel
+
+            if notes and not nomute and not ch.hold:
                 ch.note_all_off()
 
             if not ignore:
                 for n in notes:
-                    ch.note_on(p + n,127)
+                    ch.note_on(p + n, vel)
             
             ch_idx += 1
 
